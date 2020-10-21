285aa1a799472670e6a23b82920dc401523d75c5
MEDINADB <l.medina.e@hotmail.com>
2020-10-20 19:31:24 -0500

    Version 1.1

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Request;    
 use PhpParser\Node\Expr\FuncCall;
-
+use App\Product;
 class ProductController extends Controller
 {
     public function show($id){
-        return "MOSTRAR DATOS $id";
+        $product = product::find($id);
+        $images= $product -> images;
+
+        $imagesLeft= collect();
+        $imagesRight = collect();
+        foreach($images as  $key => $image){
+            if($key%2==0)
+                $imagesLeft->push($image);
+            else 
+                $imagesRight->push($image);
+        }
+        return  view('products.show')->with(compact('product','imagesLeft','imagesRight'));
     }
 }
